e44b6058af45380086bfdf13a1b567196a6430ac
Nguyễn Văn Vận <vegetaz@outlook.com>
2020-07-12 21:18:06 +0700

    Java OOP 20200712PM
    Java OOP 20200712PM

--- src/J06RelationshipBetweenObjects/Relationships48.java
+++ src/J06RelationshipBetweenObjects/Relationships48.java
@@ -1,4 +1,83 @@
 package J06RelationshipBetweenObjects;
+/*
+Lớp Point
+    - Đây là lớp dùng để lưu thông tin về một điểm trong hệ tọa độ hai chiều.
+    - x và y là 2 thuộc tính chỉ tung độ và hoành độ của một điểm.
+    - Point(x: int, y: int) là phương thức khởi tạo có tham số dùng để khởi tạo giá trị cho thuộc tính x và y.
+    - setX(), getX(), setY(), getY() là các setter và getter.
 
+Lớp Triangle
+    - Đây là lớp lưu thông tin về hình tam giác.
+    - vertice1, vertice2, vertice3 là 3 thuộc tính chỉ 3 đỉnh của tam giác.
+    - Triangle(vertice1: Point, vertice2: Point, vertice3: Point)
+      là phương thức khởi tạo có tham số dùng để khởi tạo giá trị cho 3 đỉnh trong tam giác.
+    - Triangle(x1: int, y1: int, x2: int, y2: int, x3: int, y3: int)
+      là phương thức khởi tạo có tham số dùng để khởi tạo giá trị cho 3 đỉnh trong tam giác.
+    - getPerimeter() là phương thức trả về chu vi của tam giác.
+      Ví dụ nếu vertice1 = (1, 3), vertice2 = (2, 3), vertice3 = (4, 2) thì phương thức getPerimeter() sẽ trả về 8.48528137423857.
+*/
 public class Relationships48 {
+    public static void main(String[] args) {
+        Point48 vertice1 = new Point48(1, 3);
+        Point48 vertice2 = new Point48(2, 3);
+        Point48 vertice3 = new Point48(4, 2);
+        Triangle48 triangle1 = new Triangle48(vertice1, vertice2, vertice3);
+        Triangle48 triangle2 = new Triangle48(4, 5, 2, 6, 3, 7);
+        System.out.println(triangle1.getPerimeter());
+        System.out.println(triangle2.getPerimeter());
+    }
+}
+
+class Triangle48 {
+    private Point48 vertice1;
+    private Point48 vertice2;
+    private Point48 vertice3;
+
+    public Triangle48(Point48 vertice1, Point48 vertice2, Point48 vertice3) {
+        this.vertice1 = vertice1;
+        this.vertice2 = vertice2;
+        this.vertice3 = vertice3;
+    }
+
+    public Triangle48(int x1, int y1, int x2, int y2, int x3, int y3) {
+        vertice1 = new Point48(x1, y1);
+        vertice2 = new Point48(x2, y2);
+        vertice3 = new Point48(x3, y3);
+    }
+
+    public double getPerimeter() {
+        double d1 = Math.sqrt(Math.pow(vertice1.getX() - vertice2.getX(), 2)
+                + Math.pow(vertice1.getY() - vertice2.getY(), 2));
+        double d2 = Math.sqrt(Math.pow(vertice1.getX() - vertice3.getX(), 2)
+                + Math.pow(vertice1.getY() - vertice3.getY(), 2));
+        double d3 = Math.sqrt(Math.pow(vertice2.getX() - vertice3.getX(), 2)
+                + Math.pow(vertice2.getY() - vertice3.getY(), 2));
+        return d1 + d2 + d3;
+    }
+}
+
+class Point48 {
+    private int x;
+    private int y;
+
+    public Point48(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
--- src/J06RelationshipBetweenObjects/Relationships51.java
+++ src/J06RelationshipBetweenObjects/Relationships51.java
@@ -1,4 +1,89 @@
 package J06RelationshipBetweenObjects;
 
+import java.util.ArrayList;
+import java.util.List;
+/*
+Lớp Point
+    - Đây lớp dùng để lưu thông tin về một điểm trong hệ tọa độ hai chiều.
+    - x và y là 2 thuộc tính chỉ tung độ và hoành độ của một điểm.
+    - Point(x: int, y: int) là phương thức khởi tạo có tham số dùng để khởi tạo giá trị cho thuộc tính x và y.
+    - setX(), getX(), setY(), getY() là các setter và getter.
+
+Lớp PolyLine
+    - points là thuộc tính lưu thông tin về các điểm trong PolyLine, thuộc tính này có kiểu dữ liệu là List.
+      Nếu bạn chưa học về List thì có thể tham khảo thêm tại khóa học Java cơ bản.
+    - PolyLine() là phương thức khởi tạo không tham số dùng để khởi tạo một ArrayList rỗng cho thuộc tính points.
+    - PolyLine(points: List<Point) là phương thức khởi tạo có tham số dùng để khởi tạo giá trị cho thuộc tính ponits.
+    - appendPoint(point: Point) và appendPoint(x: int, y: int) là phương thức dùng để thêm một điểm vào cuối thuộc tính points.
+    - getLength() là phương thức dùng để lấy ra độ dài của PolyLine.
+      Ví dụ nếu đối tượng PolyLine có thuộc tính points = [(1, 1), (2, 3), (3, 0), (4, 2)]
+*/
 public class Relationships51 {
+    public static void main(String[] args) {
+        PolyLine polyLine = new PolyLine();
+        polyLine.appendPoint(new Point51(1, 1));
+        polyLine.appendPoint(new Point51(2, 3));
+        polyLine.appendPoint(3, 0);
+        polyLine.appendPoint(4, 2);
+        System.out.println(polyLine.getLength());
+    }
+}
+
+class Point51 {
+    private int x;
+    private int y;
+
+    public Point51(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
+
+class PolyLine {
+    private List<Point51> points;
+
+    public PolyLine() {
+        points = new ArrayList<Point51>();
+    }
+
+    public PolyLine(List<Point51> points) {
+        this.points = points;
+    }
+
+    public void appendPoint(Point51 point) {
+        points.add(point);
+    }
+
+    public void appendPoint(int x, int y) {
+        points.add(new Point51(x, y));
+    }
+
+    public double getLength() {
+        double length = 0;
+        for (int i = 0; i < points.size() - 1; i++) {
+            int x1 = points.get(i).getX();
+            int y1 = points.get(i).getY();
+            int x2 = points.get(i + 1).getX();
+            int y2 = points.get(i + 1).getY();
+            double distance = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            length += distance;
+        }
+        return length;
+    }
 }
